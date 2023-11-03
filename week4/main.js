2978e40423d60c04e2c4c509b9dfa84b498f5238
zf-2022 <145095678+zf-2022@users.noreply.github.com>
2023-11-03 10:51:24 +0400

    done part 1

--- week4/main.js
+++ week4/main.js
@@ -10,12 +10,29 @@ btn.addEventListener("click", function () {
     }
     ourRequest.send();
 });
+
 function renderHTML(data) {
     var htmlString = "";
     for (i = 0; i < data.length; i++) {
-        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ".</p>"
-            ;
+        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: ";
+        for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+            // Loop through the indoor places of the current city.
+            if (ii == 0) {
+                htmlString += data[i].places.indoor[ii];
+            } else {
+                htmlString += ", and " + data[i].places.indoor[ii];
+            }
+        }
+        htmlString += '. & enjoy outdoor places like: ';
+        // Loop through the outdoor places of the current city.
+        for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].places.outdoor[ii];
+            } else {
+                htmlString += " and " + data[i].places.outdoor[ii];
+            }
+        }
+        htmlString += '.</p>';
     }
     cityContainer.insertAdjacentHTML('beforeend', htmlString);
-
-};
+}
