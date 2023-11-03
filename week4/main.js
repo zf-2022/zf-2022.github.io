3ad9e28f4a4da9f92c23a5d6b18c739802646135
zf-2022 <145095678+zf-2022@users.noreply.github.com>
2023-11-03 09:55:12 +0400

    fixing bug

--- week4/main.js
+++ week4/main.js
@@ -1,8 +1,22 @@
-var citiesInfo = document.getElementById("city-info");
+var cityContainer = document.getElementById("city-info");
+var button = document.getElementById("btn");
 
-citiesInfo.addEventListener("click", displayCitiesInfo())
+button.addEventListener("click", displayCitiesInfo);
 
 function displayCitiesInfo() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://github.com/zf-2022/zf-2022.github.io/blob/main/week4/cities1.json');
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+        renderHTML(ourData);
+    };
 
+    ourRequest.send();
+};
+
+function renderHTML(data) {
+    var htmlString = "this is a test";
+    cityContainer.insertAdjacentHTML('beforeend', htmlString);
 }
 
