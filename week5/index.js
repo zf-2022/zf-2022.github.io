91a86b2f20e462e66f795d7458abc34e848f64c4
zf-2022 <145095678+zf-2022@users.noreply.github.com>
2023-11-22 19:18:49 +0400

    register hbs

--- week5/index.js
+++ week5/index.js
@@ -3,16 +3,19 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 const path = require("path");
 
-dotenv.config({ path: './.env' });
+const port = 5000;
+
+//dotenv.config({ path: './week5/.env' });
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: "process.env.DATABASE_HOST",
-    user: "process.env.DATABASE_USER",
-    password: "process.env.DATABASE_PASSWORD",
-    database: "process.env.DATABASE"
+    host: 'localhost',
+    user: "root",
+    password: "",
+    database: "nodejsProject"
 });
+
 
 db.connect((error) => {
     if (error) {
@@ -23,12 +26,17 @@ db.connect((error) => {
 });
 
 const pubilcDriectory = path.join(__dirname, './public');
+app.use(express.static(pubilcDriectory));
 app.set('view engine', 'hbs');
 
-const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send("<h1> HELLO WORLD </h1>")
+    //res.send("<h1> HELLO WORLD </h1>")
+    res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
 });
 
 app.listen(port, () => {
